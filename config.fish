12b5cca5819db4508e738be1dcfe3a110bfb35b1
Silvan Kaiser <silvan@quobyte.com>
2021-07-23 13:10:19 +0200

    Fix failing process detection for ssh-agent in fish

--- config.fish
+++ config.fish
@@ -7,7 +7,7 @@ alias k8s='/usr/bin/kubectl'
 alias ll='ls -lash'
 alias gitpowerpull='git pull --all --tags --force; and git submodule sync ; and git submodule update --init --recursive'
 
-if test -z (pgrep ssh-agent | string collect)
+if test -z (pgrep -x ssh-agent)
     eval (ssh-agent -c)
     set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
     set -Ux SSH_AGENT_PID $SSH_AGENT_PID
