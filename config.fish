f8cd453804ee57b287ecad0e29d1dcd434e4f650
Silvan Kaiser <silvan@quobyte.com>
2021-07-28 17:46:53 +0200

    Do not leak pid when multiple ssh-agent processes are running.

--- config.fish
+++ config.fish
@@ -7,7 +7,7 @@ alias k8s='/usr/bin/kubectl'
 alias ll='ls -lash'
 alias gitpowerpull='git pull --all --tags --force; and git submodule sync ; and git submodule update --init --recursive'
 
-if test -z (pgrep -x ssh-agent)
+if test -z (pgrep -x ssh-agent|tr -d '[:space:]')
     eval (ssh-agent -c)
     set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
     set -Ux SSH_AGENT_PID $SSH_AGENT_PID
